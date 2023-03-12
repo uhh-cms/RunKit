886246d9bf4c53c96d7b579768598e6a41b00ee6
Konstantin <konstantin.androsov@gmail.com>
2023-03-12 22:12:15 +0100

    Added support for a generic cmsRun jobs.

---
+++ cmsRunWrapper.py
@@ -0,0 +1,62 @@
+# Crab wrapper.
+
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing('analysis')
+options.register('cmsRunCfg', '', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                 "Path to the cmsRun python configuration file.")
+options.register('cmsRunOptions', '', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                 "Comma separated list of options that should be passed to cmsRun.")
+options.register('skimCfg', '', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                 "Skimming configuration in YAML format.")
+options.register('storeFailed', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
+                 "Store minimal information about events that failed selection.")
+options.register('mustProcessAllInputs', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
+                 "To sucessfully finish, all inputs must be processed.")
+options.register('createTar', True, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
+                 "Create a tar file with all outputs.")
+options.register('maxRuntime', 20, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                 "Maximal expected job runtime in hours.")
+options.register('maxFiles', -1, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                 "Maximal number of files to process.")
+options.register('customiseCmds', '', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                 "Production customization commands (if any)")
+options.register('writePSet', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
+                 "Dump configuration into PSet.py.")
+options.register('copyInputsToLocal', True, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
+                 "Copy inputs (one at the time) to a job working directory before processing them.")
+options.register('output', 'nano.root', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                 "Name of the output file.")
+options.register('datasetFiles', '', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                 """Path to a JSON file with the dict of all files in the dataset.
+                    It is used to assing file ids to the outputs.
+                    If empty, indices of input files as specified in inputFiles are used.""")
+
+options.parseArguments()
+
+process = cms.Process('cmsRun')
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(options.inputFiles))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+if options.maxEvents > 0:
+  process.maxEvents.input = options.maxEvents
+process.exParams = cms.untracked.PSet(
+  cmsRunCfg = cms.untracked.string(options.cmsRunCfg),
+  cmsRunOptions = cms.untracked.string(options.cmsRunOptions),
+  skimCfg = cms.untracked.string(options.skimCfg),
+  storeFailed = cms.untracked.bool(options.storeFailed),
+  customisationCommands = cms.untracked.string(options.customiseCmds),
+  createTar = cms.untracked.bool(options.createTar),
+  mustProcessAllInputs = cms.untracked.bool(options.mustProcessAllInputs),
+  maxRuntime = cms.untracked.int32(options.maxRuntime),
+  jobModule = cms.untracked.string('crabJob_cmsRun.py'),
+  output = cms.untracked.string(options.output),
+  datasetFiles = cms.untracked.string(options.datasetFiles),
+  maxFiles = cms.untracked.int32(options.maxFiles),
+  copyInputsToLocal = cms.untracked.bool(options.copyInputsToLocal),
+)
+
+if options.writePSet:
+  with open('PSet.py', 'w') as f:
+    print(process.dumpPython(), file=f)
---
+++ crabJob_cmsRun.py
@@ -0,0 +1,99 @@
+import shutil
+import os
+import yaml
+
+from sh_tools import sh_call
+
+def getFilePath(file):
+  if os.path.exists(file):
+    return file
+  file_name = os.path.basename(file)
+  if os.path.exists(file_name):
+    return file_name
+  if 'ANALYSIS_PATH' in os.environ:
+    file_path = os.path.join(os.environ['ANALYSIS_PATH'], file)
+    if os.path.exists(file_path):
+      return file_path
+  if 'CMSSW_BASE' in os.environ:
+    file_path = os.path.join(os.environ['CMSSW_BASE'], 'src', file)
+    if os.path.exists(file_path):
+      return file_path
+  raise RuntimeError(f"Unable to find {file}")
+
+def processFile(input_file, output_file, tmp_files, cmssw_report, cmd_line_args, cfg_params):
+  run_cmsRun = True
+  store_failed = cfg_params.storeFailed
+  skim_cfg = cfg_params.skimCfg
+  run_skim = len(skim_cfg) > 0
+  debug = len(cmd_line_args) > 0 and cmd_line_args[0] == 'DEBUG'
+
+  if debug:
+    if len(cmd_line_args) > 1:
+      run_cmsRun = cmd_line_args[1] == 'True'
+    if len(cmd_line_args) > 2:
+      run_skim = cmd_line_args[2] == 'True'
+    if len(cmd_line_args) > 3:
+      store_failed = cmd_line_args[3] == 'True'
+
+  if run_cmsRun:
+    cmsRunCfg = cfg_params.cmsRunCfg
+    cmsRunOptions = cfg_params.cmsRunOptions.split(',') if len(cfg_params.cmsRunOptions) > 0 else []
+
+    customise_commands = cfg_params.customisationCommands
+    cfg_name = cmsRunCfg
+    if len(customise_commands) > 0:
+      cfg_name = f'{cmsRunCfg}_{output_file}.py'
+      shutil.copy(cmsRunCfg, cfg_name)
+      tmp_files.append(cfg_name)
+      with open(cfg_name, 'a') as f:
+        f.write('\n' + customise_commands)
+
+    cmssw_cmd = [ 'cmsRun',  '-j', cmssw_report, cfg_name, f'inputFiles={input_file}', f'output={output_file}',
+                  f'maxEvents={cfg_params.maxEvents}' ]
+    cmssw_cmd.extend(cmsRunOptions)
+    sh_call(cmssw_cmd, verbose=1)
+
+  if run_skim:
+    output_name, output_ext = os.path.splitext(output_file)
+    cmsRun_out = output_name + '_cmsRun' + output_ext
+    shutil.move(output_file, cmsRun_out)
+    skim_tree_path = os.path.join(os.path.dirname(__file__), 'skim_tree.py')
+    with open(skim_cfg, 'r') as f:
+      skim_config = yaml.safe_load(f)
+
+    cmd_line = ['python3', skim_tree_path, '--input', cmsRun_out, '--output', output_file,
+                '--input-tree', skim_config['input_tree'], '--verbose', '1']
+
+    if 'other_trees' in skim_config:
+      cmd_line.extend(['--other-trees', ','.join(skim_config['other_trees'])])
+
+    if 'selection' in skim_config:
+      selection = skim_config['selection']
+      cmd_line.extend(['--sel', selection])
+
+    if 'processing_module' in skim_config:
+      proc_module = skim_config['processing_module']
+      cmd_line.extend(['--processing-module', getFilePath(proc_module['file']) + ':' + proc_module['function']])
+
+    if 'column_filters' in skim_config:
+      columns = ','.join(skim_config['column_filters'])
+      cmd_line.extend([f'--column-filters', columns])
+
+    sh_call(cmd_line, verbose=1)
+
+    if store_failed:
+      cmd_line = ['python3', skim_tree_path, '--input', cmsRun_out, '--output', output_file,
+                  '--input-tree', skim_config['input_tree'], '--output-tree', skim_config['output_failed_tree'], '--update-output', '--verbose', '1']
+
+      if 'selection' in skim_config:
+        cmd_line.extend(['--invert-sel', '--sel', selection])
+
+      if 'processing_module_for_failed' in skim_config:
+        proc_module = skim_config['processing_module_for_failed']
+        cmd_line.extend(['--processing-module', getFilePath(proc_module['file']) + ':' + proc_module['function']])
+
+      if 'column_filters_for_failed' in skim_config:
+        columns = ','.join(skim_config['column_filters_for_failed'])
+        cmd_line.extend([f'--column-filters', columns])
+
+      sh_call(cmd_line, verbose=1)
